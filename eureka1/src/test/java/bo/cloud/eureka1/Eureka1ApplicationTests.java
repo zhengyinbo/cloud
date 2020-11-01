1c504c11d831a1f340fa3479195821b21d1027f7
bo <634946905@qq.com>
2020-11-02 07:53:15 +0800

    commit code

---
+++ eureka1/src/test/java/bo/cloud/eureka1/Eureka1ApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.cloud.eureka1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eureka1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
